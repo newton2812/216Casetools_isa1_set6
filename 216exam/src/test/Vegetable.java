297ace012a002c4ba077c69b7cfddf5ed967bab5
newton2812 <newtonvital2819@gmail.com>
2024-02-10 11:05:13 +0530

    vegetable

--- 216exam/src/test/Vegetable.java
+++ 216exam/src/test/Vegetable.java
@@ -2,5 +2,19 @@ package test;
 
 public class Vegetable {
 
+		
+		String name;
+		Vegetable(String name)
+		{
+			this.name=name;
+	}
+		{
+		Vegetable spinach=new Vegetable("spinach");
+		System.out.println("Vegetrable is" +spinach.name);
 	
+}
+		String getType() {
+			return name;
+		}
+		
 }
